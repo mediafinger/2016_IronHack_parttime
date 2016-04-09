9985bb99918bf9fd7b919c3d09c61ec03c8b9d7d
Andreas Finger <webmaster@mediafinger.com>
2016-04-09 12:19:00 +0200

    Add income tax and fake notification

--- payroll.rb
+++ payroll.rb
@@ -1,11 +1,32 @@
 class Payroll
+  INCOME_TAX = 18.0
+
   def initialize(employees)
     @employees = employees
   end
 
   def pay_employees
     @employees.each do |employee|
-      puts "#{employee.name} => #{employee.calculate_salary}"
+      salary = employee.calculate_salary * (1 + INCOME_TAX / 100)
+
+      puts "#{employee.name} => #{salary}"
+
+      notify_employees(employee, salary)
     end
   end
+
+  def notify_employees(employee, salary)
+    # Email.send(
+    #   subject: "Payment sent",
+    #   recipient: employee.name,
+    #   email: employee.email,
+    #   body: "Hello we paid you #{salary}"
+    # )
+  end
 end
+
+#
+# class Email
+#   def self.send(subject:, recipient:, email:, body:)
+#   end
+# end
