ccfd6d73abd482e05c52d8f3cd38849198085c56
Andreas Finger <webmaster@mediafinger.com>
2016-04-13 01:19:56 +0200

    Extract show_menu, counter and transformer methods
    into new StringOperations class to demonstrate the
    second of the SOLID principles: open-closed

--- login.rb
+++ login.rb
@@ -1,8 +1,20 @@
-require_relative "./counter.rb"
-require_relative "./transformer.rb"
+require_relative "./string_operations.rb"
 
 class Login
+  # open-closed principle example
+  # you could now inherit from Login into a class MyLogin
+  # and only overwrite the initialize method to use other operations
+  #
+  # class MyLogin < Login
+  #   def initialize
+  #     @operations = MathOperations.new
+  #   end
+  # end
+  #
+  # this way the class is open for extension, but closed for modifications
+  #
   def initialize
+    @operations = StringOperations.new
   end
 
   def prompt
@@ -15,39 +27,11 @@ class Login
     # just comment the line above and uncomment
     # the line below to use a different implementation
     # if User.new.valid?(name, password)
-      show_menu
+
+      @operations.show_menu
     else
       puts "Try again"
       prompt
     end
-  end
-
-  private
-
-  def show_menu
-    puts "Please select:"
-    puts "A) word count"
-    puts "B) letter count"
-    puts "C) reverse string"
-    puts "D) to lowercase"
-    puts "E) to uppercase"
-
-    selection = gets.chomp.downcase
-
-    case selection
-    when "a"
-      puts Counter.count_words
-    when "b"
-      puts Counter.count_characters
-    when "c"
-      puts Transformer.reverse
-    when "d"
-      puts Transformer.downcase
-    when "e"
-      puts Transformer.uppercase
-    end
-
-    # always display the menu again
-    show_menu
   end
 end
