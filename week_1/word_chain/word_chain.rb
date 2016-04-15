c31a1b2116e72d7b9dfeb8248f7642d61c8dc7f4
Andreas Finger <webmaster@mediafinger.com>
2016-04-15 10:59:30 +0200

    Refactor WordChain to allow only existing words
    This version uses the ruby-dictionary gem

--- week_1/word_chain/word_chain.rb
+++ week_1/word_chain/word_chain.rb
@@ -6,18 +6,31 @@ class WordChain
   end
 
   def find_chain(start_word, end_word)
-    temp_word = start_word
+    chain_word = start_word
+    puts chain_word
 
-    start_word.chars.each_with_index do |_char, i|
-      puts temp_word
-      temp_word[i] = end_word[i]
+    while chain_word != end_word
+      end_word.chars.each_with_index do |char, i|
+        temp_word = chain_word.dup # we have to use a dup / Duplicate of the String
+        temp_word[i] = char
+
+        if find_word(temp_word)
+          puts temp_word if temp_word != chain_word
+          chain_word = temp_word
+        end
+      end
     end
+  end
 
-    puts end_word
+  def find_word(word)
+    @dictionary.exists?(word)
   end
 end
+
+# instantiate and execute the code:
 
 dictionary = Dictionary.from_file("/usr/share/dict/words")
 chain = WordChain.new(dictionary)
 
 chain.find_chain("cat", "dog")
+chain.find_chain("code", "ruby")
