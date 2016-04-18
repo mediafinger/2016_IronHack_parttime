403a162bf1760c048d19f7c15e851790d24ea4d8
Andreas Finger <webmaster@mediafinger.com>
2016-04-18 14:54:50 +0200

    ChessValidator: add tests for Piece class

--- week_2/chess_validator/models/piece.rb
+++ week_2/chess_validator/models/piece.rb
@@ -12,6 +12,6 @@ class Piece
   # the class that inherits from Piece
   # the class Piece itself should not be instantiated
   def move_valid?(_new_position)
-    fail "MethodNotImplementedError"
+    fail MethodNotImplementedError
   end
 end
---
+++ week_2/chess_validator/spec/models/piece_spec.rb
@@ -0,0 +1,25 @@
+describe Piece do
+  before do
+    @piece = Piece.new(:white, "a1")
+  end
+
+  describe ".new" do
+    it "instantiates a Piece object" do
+      expect(@piece.class).to eql Piece
+    end
+
+    it "sets the color to white" do
+      expect(@piece).to have_attributes(:color => :white)
+    end
+
+    it "sets the color to black" do
+      expect(Piece.new(:black, "f2")).to have_attributes(:color => :black)
+    end
+  end
+
+  describe "#move_valid?" do
+    it "is not implemented and throws an error" do
+      expect { @piece.move_valid?("a2") }.to raise_error MethodNotImplementedError
+    end
+  end
+end
