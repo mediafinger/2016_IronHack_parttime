52dedd86dafddc7688879f0465d8283f8706ba33
Andreas Finger <webmaster@mediafinger.com>
2016-05-27 16:51:54 +0200

    Timetracking app: add class method clean_old! to Project

--- week_7/timetracking/app/models/project.rb
+++ week_7/timetracking/app/models/project.rb
@@ -1,3 +1,9 @@
 class Project < ActiveRecord::Base
   validates :name, length: { minimum: 3 }
+
+  def self.clean_old!
+    projects = Project.where("created_at < ?", Date.today - 1.week)
+
+    projects.destroy_all
+  end
 end
