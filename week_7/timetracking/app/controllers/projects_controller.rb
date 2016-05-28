fe01a60c263af006358c328a751274789a91b17a
Andreas Finger <webmaster@mediafinger.com>
2016-05-28 12:10:02 +0200

    Timetracking app: add projects#create post action

--- week_7/timetracking/app/controllers/projects_controller.rb
+++ week_7/timetracking/app/controllers/projects_controller.rb
@@ -16,4 +16,10 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    project = Project.new(project_params)
+    project.save
+    redirect_to projects_path
+  end
 end
