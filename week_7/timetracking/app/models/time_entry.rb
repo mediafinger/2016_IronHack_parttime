44f2a5a96441eafdcd936585f3e7ff4beee3911d
Andreas Finger <webmaster@mediafinger.com>
2016-06-03 19:15:18 +0200

    Timetracking app: add validations for TimeEntry

--- week_7/timetracking/app/models/time_entry.rb
+++ week_7/timetracking/app/models/time_entry.rb
@@ -1,3 +1,6 @@
 class TimeEntry < ActiveRecord::Base
   belongs_to :project
+
+  validates :hours, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
+  validates :minutes, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }
 end
