69ac52fc058cc2dafeec183a6477938b4a3760f5
Andreas Finger <webmaster@mediafinger.com>
2016-06-04 12:00:58 +0200

    Timetracking app: extract flash to helper

--- week_7/timetracking/app/helpers/application_helper.rb
+++ week_7/timetracking/app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def formatted_date(date)
     date.strftime("%d %b %Y")
   end
+
+  # TODO exercise: refactor method, keep it DRY
+  def flash_message
+    if flash[:alert]
+      content_tag :div, class: "flash alert" do
+        flash[:alert]
+      end
+    elsif flash[:notice]
+      content_tag :div, class: "flash notice" do
+        flash[:notice]
+      end
+    end
+  end
 end
