c0a418424db7718c027375850ab654a30f6e8040
Andreas Finger <webmaster@mediafinger.com>
2016-06-25 15:57:49 +0200

    Timetracking app emails: bundle gem letter_opener

--- week_7/timetracking/config/environments/development.rb
+++ week_7/timetracking/config/environments/development.rb
@@ -16,6 +16,10 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # if you use the gem letter_opener, set it as delivery_method
+  # instead of sending email, it will open them in a new browser tab
+  # config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
