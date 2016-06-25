c4cb25e66520e44bd546456941078eeb8351e067
Andreas Finger <webmaster@mediafinger.com>
2016-06-25 11:29:11 +0200

    Timetracking app: add missing helper method current_user

--- week_7/timetracking/app/controllers/application_controller.rb
+++ week_7/timetracking/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
 
   def log_error(options)
     string = "*****\n"
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
     string += "with params: #{options[:params]}\n"
     string += "*****\n"
     Rails.logger.warn(string)
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
