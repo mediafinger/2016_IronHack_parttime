4395d832c44c94f598c2414ef50c5271f36677cb
Andreas Finger <webmaster@mediafinger.com>
2016-06-25 16:05:15 +0200

    Timetracking app email: add SMTP configuration for GMAIL

--- week_7/timetracking/config/environments/development.rb
+++ week_7/timetracking/config/environments/development.rb
@@ -14,11 +14,22 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # if you use the gem letter_opener, set it as delivery_method
   # instead of sending email, it will open them in a new browser tab
   # config.action_mailer.delivery_method = :letter_opener
+
+  # GMAIL settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            ENV["GMAIL_IRONHACK_USERNAME"],
+    password:             ENV["GMAIL_IRONHACK_PASSWORD"],
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
