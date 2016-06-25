4874fc566f26f0b9a1c732cefe6e220c8029e443
Andreas Finger <webmaster@mediafinger.com>
2016-06-25 11:48:17 +0200

    Timetracking app: add authenticate! action to ensure a logged-in user

--- week_7/timetracking/app/controllers/application_controller.rb
+++ week_7/timetracking/app/controllers/application_controller.rb
@@ -15,4 +15,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  def authenticate!
+    redirect_to "/login" unless current_user.present?
+  end
 end
--- week_7/timetracking/app/controllers/projects_controller.rb
+++ week_7/timetracking/app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate!, only: [:new, :create]
+
   def index
     @projects = Project.latest
   end
